f12845fbf5840402a32ced545dc159cb48d3144d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-11-22 15:42:48 +0800

    first:realize filtering no repeat number

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,7 +1,36 @@
 function single_element(arr) {
-    var result = arr.filter(function(item, i) {
+    var filterNum = arr.filter(function(item, i) {
         return i % 2 === 1;
     });
+    var objectArray = getObjectArray(filterNum);
+    var result = [];
 
+    objectArray.forEach(function(n, i) {
+        if (n.count === 1) {
+            result.push(n.num);
+        }
+    });
     return result;
+}
+
+function getObjectArray(arr) {
+    var objectArray = [];
+
+    arr.forEach(function(n, i) {
+        var exist = false;
+
+        objectArray.forEach(function (m, j) {
+            if(n === m.num) {
+                m.count ++;
+                exist = true;
+            }
+        });
+        if(!exist) {
+            objectArray.push({
+                num: n,
+                count: 1
+            });
+        }
+    });
+    return objectArray;
 }
